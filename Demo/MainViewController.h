75ac067c91285e879f1f00a804e844ca90ba5dca
杜振 <181138630@qq.com>
2016-10-20 23:20:36 +0800

    新建了一个控制器

---
+++ Demo/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Demo
+//
+//  Created by Apple on 16/10/20.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Demo/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  Demo
+//
+//  Created by Apple on 16/10/20.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"初始化项目");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
